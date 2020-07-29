674042395ebd135f98e3730bc5ed006dd9fe1f2e
marcos-victor <marcos-victor@pc-021>
2020-07-29 10:11:27 -0300

    correção de bugs

--- src/modelo/dao/HibernateUtil.java
+++ src/modelo/dao/HibernateUtil.java
@@ -1,3 +1,8 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package modelo.dao;
 
 import org.hibernate.cfg.AnnotationConfiguration;
@@ -7,7 +12,7 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author Marcos Victor
+ * @author marcos-victor
  */
 public class HibernateUtil {
 
